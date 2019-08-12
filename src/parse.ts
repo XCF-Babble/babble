10347a0860628eac79adaaa5c5f0b74bb1b8ae14
Kevin Kuehler <keur@ocf.berkeley.edu>
2019-08-12 02:27:04 -0700

    initial commit

---
+++ src/parse.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+export const walkDOM = (root: Element, callback: (elem: Element) => void ) => {
+  parseDOM(root, callback);
+}
+
+const parseDOM = (root: Element, callback: (elem: Element) => void ) => {
+  for (var i = 0; i < root.children.length; i++) {
+    console.log("parsing...")
+    const child: Element = root.children[i];
+    callback(child);
+    parseDOM(child, callback);
+  }
+}
