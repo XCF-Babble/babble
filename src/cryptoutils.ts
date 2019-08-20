71beafb856d13beb5ce99e300715d049a5455237
Kevin Kuehler <keur@ocf.berkeley.edu>
2019-08-20 12:56:55 -0700

    Port the rest of original babble crypto utils

--- src/cryptoutils.ts
+++ src/cryptoutils.ts
@@ -1,7 +1,9 @@
 'use strict';
 
 const _sodium = require('libsodium-wrappers');
-const babbleSalt = 'BabbleBabbleBabb';
+const babbleSalt: string = 'BabbleBabbleBabb';
+const babbleBase: string =
+  '的一是了人在有我他这为之来以个们到说和地也子时而要于就下得可你年生自会那后能对着事其里所去行过十用发如然方成者多都三二同么当起与好看学进将还分此心前面又定见只从现因开些长明样已月正想实把相两力等外它并间手应全门点身由何向至物被五及入先己或很最书美山什名曰合加世水果位度马给数次今表原各才几解则气再听提万更比百尔即白许系且光路接结题指感量取场电空边件住放风求形望传笑叫往区达设记字故品象花七服据云像飞远收石类程转千式流每该始术格运怎步让识拉若备造快集布尽称确呢节注存具甚容吃算坐早引似视尚轻况留照写足余星居技属找食';
 
 export const libsodium = async () => {
   const sodium = _sodium;
@@ -22,4 +24,94 @@ export const deriveKey = async (
     sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE,
     sodium.crypto_pwhash_ALG_DEFAULT
   );
+};
+
+export const encrypt = async (
+  s: string,
+  babbleKey: Uint8Array
+): Promise<Uint8Array> => {
+  const sodium = await libsodium();
+  const nonce: Uint8Array = sodium.randombytes_buf(
+    sodium.crypto_aead_chacha20poly1305_ietf_NPUBBYTES
+  );
+  const cipher: Uint8Array = sodium.crypto_aead_chacha20poly1305_ietf_encrypt(
+    sodium.from_string(s),
+    null,
+    null,
+    nonce,
+    babbleKey
+  );
+  const ret: Uint8Array = new Uint8Array(nonce.length + cipher.length);
+  ret.set(nonce);
+  ret.set(cipher, nonce.length);
+  return ret;
+};
+
+export const decrypt = async (
+  s: Uint8Array,
+  babbleKey: Uint8Array
+): Promise<string> => {
+  const sodium = await libsodium();
+  if (
+    s.length <
+    sodium.crypto_aead_chacha20poly1305_ietf_NPUBBYTES +
+      sodium.crypto_aead_chacha20poly1305_ietf_ABYTES
+  )
+    return '';
+  const nonce: Uint8Array = s.subarray(
+    0,
+    sodium.crypto_aead_chacha20poly1305_ietf_NPUBBYTES
+  );
+  const cipher: Uint8Array = s.subarray(
+    sodium.crypto_aead_chacha20poly1305_ietf_NPUBBYTES
+  );
+  return sodium.to_string(
+    sodium.crypto_aead_chacha20poly1305_ietf_decrypt(
+      null,
+      cipher,
+      null,
+      nonce,
+      babbleKey
+    )
+  );
+};
+
+export const hanziEncode = async (s: Uint8Array): Promise<string> => {
+  var ret = '';
+  s.forEach(b => {
+    ret += babbleBase[b];
+  });
+  return ret;
+};
+
+export const hanziDecode = async (s: string): Promise<Uint8Array> => {
+  var ret = new Uint8Array(s.length);
+  var parsed = 0;
+  for (var c of s) {
+    var index = babbleBase.indexOf(c);
+    if (index != -1) ret[parsed++] = index;
+  }
+  return ret.subarray(0, parsed);
+};
+
+export const babble = async (
+  s: string,
+  babbleKey: Uint8Array
+): Promise<string> => {
+  try {
+    return await hanziEncode(await encrypt(s, babbleKey));
+  } catch (e) {
+    return '';
+  }
+};
+
+export const debabble = async (
+  s: string,
+  babbleKey: Uint8Array
+): Promise<string> => {
+  try {
+    return await decrypt(await hanziDecode(s), babbleKey);
+  } catch (e) {
+    return '';
+  }
 };
