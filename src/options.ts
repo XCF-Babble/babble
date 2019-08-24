406de2d78355a3c40db862865973a4669bd28850
Kevin Kuehler <keur@ocf.berkeley.edu>
2019-08-23 22:00:30 -0700

    UI cleanup
    * Use javascript templates
    * Escape user input (XSS)
    * Disable button after click due to argon taking a while

--- src/options.ts
+++ src/options.ts
@@ -25,47 +25,37 @@ const refreshTable = async (): Promise<void> => {
         continue;
       }
     }
-    var row: string = '';
-    row += '<tr>';
-    row += '<td>';
-    var selectButtonId = '';
+    var button: string = '';
+    var truncatedBase: string = '';
+    var tags: string = '';
+    var editButton: string = '';
+    var deleteButton: string = '';
+    var selectButtonId: string = '';
+    var deleteButtonId: string = '';
+
     if (i == selectedEntry) {
-      row += 'Selected';
+      button = 'Selected';
     } else {
       selectButtonId = 'selectButton' + i.toString();
-      row +=
-        '<button type="button" class="btn btn-primary btn-sm btn-block" id="' +
-        selectButtonId +
-        '">Select</button>';
+      button = `<button type="button" class="btn btn-primary btn-sm btn-block" id="${selectButtonId}">Select</button>`;
     }
-    row += '</td>';
-    row += '<td>';
-    row += entry.name;
-    row += '</td>';
-    row += '<td>';
-    row += entry.passphrase;
-    row += '</td>';
-    row += '<td>';
-    row += entry.base.substr(0, 10) + '...';
-    row += '</td>';
-    row += '<td>';
-    row += entry.tags.join(', ');
-    row += '</td>';
-    row += '<td>';
+    truncatedBase = entry.base.substr(0, 10) + '...';
+    tags = entry.tags.join(', ');
     const editButtonId = 'editButton' + i.toString();
-    row +=
-      '<button type="button" class="btn btn-info btn-sm btn-block" id="' +
-      editButtonId +
-      '">Edit</button>';
-    row += '</td>';
-    row += '<td>';
-    const deleteButtonId = 'deleteButton' + i.toString();
-    row +=
-      '<button type="button" class="btn btn-danger btn-sm btn-block" id="' +
-      deleteButtonId +
-      '">Delete</button>';
-    row += '</td>';
-    row += '</tr>';
+    editButton = `<button type="button" class="btn btn-info btn-sm btn-block" id="${editButtonId}">Edit</button>`;
+    deleteButtonId = 'deleteButton' + i.toString();
+    deleteButton = `<button type="button" class="btn btn-danger btn-sm btn-block" id="${deleteButtonId}">Delete</button>`;
+    const row = `
+    <tr>
+      <td>${button}</td>
+      <td>${escapeHtml(entry.name)}</td>
+      <td>${escapeHtml(entry.passphrase)}</td>
+      <td>${escapeHtml(truncatedBase)}</td>
+      <td>${escapeHtml(tags)}</td>
+      <td>${editButton}</td>
+      <td>${deleteButton}</td>
+    </tr>
+    `;
     tbody.append(row);
     const id = i;
     if (selectButtonId !== '') {
@@ -107,6 +97,7 @@ $(document).ready(async () => {
     await refreshTable();
   });
   $('#saveButton').click(async () => {
+    $('#saveButton').prop('disabled', true); // spammy clickers can't create multiple keys
     const editId = $('#editId').val();
     const name = $('#nameBox').val();
     const passphrase = $('#passphraseBox').val();
@@ -137,6 +128,26 @@ $(document).ready(async () => {
     }
     $('#entryModal').modal('hide');
     await refreshTable();
+    $('#saveButton').prop('disabled', false);
   });
   $('#searchBox').keyup(refreshTable);
 });
+
+// Taken from mustache.js templating library
+// https://github.com/janl/mustache.js/blob/6c3608bfb9fa74684cd9e22f5bb4c097f87484ef/mustache.js#L73-L88
+const entityMap: { [unsafeEntity: string]: string } = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '/': '&#x2F;',
+  '`': '&#x60;',
+  '=': '&#x3D;'
+};
+
+const escapeHtml = (unsafe: string): string => {
+  return unsafe.replace(/[&<>"'`=\/]/g, (s: string): string => {
+    return entityMap[s];
+  });
+};
