879bdca6cdb54565b18b0a3533b793e47fa0b73c
Kevin Kuehler <keur@xcf.berkeley.edu>
2019-09-17 01:19:51 -0700

    lint: Actually lint nested files
    the tsconfig.json has all the necessary information. Adding src/**/*.ts
    caused the linter to only go one directory deep. Oops.

--- src/content/websites/discord.ts
+++ src/content/websites/discord.ts
@@ -24,42 +24,42 @@ import { documentObserver, sendEnterEvent } from '../../utils/webutils';
 
 export class Discord extends Website {
   private targetElement: HTMLTextAreaElement | null;
-  constructor() {
+  constructor () {
     super();
     this.domain = 'discordapp.com';
     this.targetElement = null;
   }
 
-  register(): void {
-    documentObserver((mutationsList: MutationRecord[], observer: MutationObserver) => {
-      const inputBoxes: HTMLCollectionOf<HTMLTextAreaElement> = document.getElementsByTagName('textarea');
-      if (inputBoxes.length > 0) {
+  register (): void {
+    documentObserver( ( mutationsList: MutationRecord[], observer: MutationObserver ) => {
+      const inputBoxes: HTMLCollectionOf<HTMLTextAreaElement> = document.getElementsByTagName( 'textarea' );
+      if ( inputBoxes.length > 0 ) {
         this.targetElement = inputBoxes[0];
       }
-    });
-  };
+    } );
+  }
 
-  tunnelInput(s: string): boolean {
-    if (!this.targetElement) {
+  tunnelInput ( s: string ): boolean {
+    if ( !this.targetElement ) {
       return false;
     }
     this.targetElement.value = s;
     // React tracks DOMNode.value changes, so we also need to fire an 'input' event.
     // https://github.com/facebook/react/issues/10135#issuecomment-314441175
-    this.targetElement.dispatchEvent(new KeyboardEvent('input', { bubbles: true}));
+    this.targetElement.dispatchEvent( new KeyboardEvent( 'input', { bubbles: true } ) );
     return true;
   }
 
-  submitInput(): boolean {
-    if (!this.targetElement) {
+  submitInput (): boolean {
+    if ( !this.targetElement ) {
       return false;
     }
-    sendEnterEvent('keypress', this.targetElement);
+    sendEnterEvent( 'keypress', this.targetElement );
     return true;
   }
 
-  clearInput(): boolean {
-    if (!this.targetElement) {
+  clearInput (): boolean {
+    if ( !this.targetElement ) {
       return false;
     }
     this.targetElement.value = '';
--- src/content/websites/slack.ts
+++ src/content/websites/slack.ts
@@ -24,37 +24,37 @@ import { documentObserver, sendEnterEvent } from '../../utils/webutils';
 
 export class Slack extends Website {
   private targetElement: HTMLElement | null;
-  constructor() {
+  constructor () {
     super();
     this.domain = 'app.slack.com';
     this.targetElement = null;
   }
 
-  register(): void {
-    documentObserver((mutationsList: MutationRecord[], observer: MutationObserver) => {
-      this.targetElement = document.querySelector('div.ql-editor');
-    });
-  };
+  register (): void {
+    documentObserver( ( mutationsList: MutationRecord[], observer: MutationObserver ) => {
+      this.targetElement = document.querySelector( 'div.ql-editor' );
+    } );
+  }
 
-  tunnelInput(s: string): boolean {
-    if (!this.targetElement) {
+  tunnelInput ( s: string ): boolean {
+    if ( !this.targetElement ) {
       return false;
     }
     this.targetElement.innerText = s;
     return true;
   }
 
-  submitInput(): boolean {
-    if (!this.targetElement) {
+  submitInput (): boolean {
+    if ( !this.targetElement ) {
       return false;
     }
-    (this.targetElement.parentElement as HTMLElement).click();
-    sendEnterEvent('keydown', this.targetElement);
+    ( this.targetElement.parentElement as HTMLElement ).click();
+    sendEnterEvent( 'keydown', this.targetElement );
     return true;
   }
 
-  clearInput(): boolean {
-    if (!this.targetElement) {
+  clearInput (): boolean {
+    if ( !this.targetElement ) {
       return false;
     }
     this.targetElement.innerHTML = '';
