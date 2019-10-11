623cad23522f64dabf7c53a9843785a0a93b43e2
yvbbrjdr <xyyqzy@hotmail.com>
2019-10-11 08:36:25 -0700

    Remove repeated character in the url regex

--- src/web_accessible_resources/decrypt.ts
+++ src/web_accessible_resources/decrypt.ts
@@ -143,7 +143,7 @@ window.onload = (): void => {
 const urlify = ( text: string ): string => {
   // Taken from https://github.com/sindresorhus/linkify-urls/blob/62fd87c59d61eb8d15530e4d38dbc99abdef78b6/index.js#L5
   // and modified to include escaped characters.
-  const urlRegex = /^((?:https?(?:(?::\/\/)|(?::&#x2F;&#x2F;)))(?:www\.)?(?:[a-zA-Z\d-_.]+(?:(?:\.|@)[a-zA-Z\d]{2,})|localhost)(?:(?:[-a-zA-Z\d:%_+.~#!?&//=@]*(?:(?:&#x2F;)|(?:&amp;))*)(?:[,](?![\s]))*)*)$/;
+  const urlRegex = /^((?:https?(?:(?::\/\/)|(?::&#x2F;&#x2F;)))(?:www\.)?(?:[a-zA-Z\d-_.]+(?:(?:\.|@)[a-zA-Z\d]{2,})|localhost)(?:(?:[-a-zA-Z\d:%_+.~#!?&/=@]*(?:(?:&#x2F;)|(?:&amp;))*)(?:[,](?![\s]))*)*)$/;
   const words = text.split( ' ' );
   words.forEach( ( _: string, i: number ) => {
     words[i] = _.replace( urlRegex, ( url: string ) => {
